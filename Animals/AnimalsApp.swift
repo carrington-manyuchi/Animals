c206eaa04c45c4b65d07dd0ab4930f0281bf9ef9
Carrington Manyuchi <carrington.manyuchi@standardbank.co.za>
2025-05-06 05:14:34 +0200

    Initial Commit

---
+++ Animals/AnimalsApp.swift
@@ -0,0 +1,17 @@
+//
+//  AnimalsApp.swift
+//  Animals
+//
+//  Created by Manyuchi, Carrington C on 2025/05/06.
+//
+
+import SwiftUI
+
+@main
+struct AnimalsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
